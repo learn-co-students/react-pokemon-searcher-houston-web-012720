a2daae51104017d7c43db608be0997eb91d2b03d
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-04-06 10:44:18 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,24 +2,63 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
-  render() {
+  
+  constructor() {
+    super()
+    this.state = {
+      showFace: true
+    }
+  }
+
+  changeState = () => this.setState({showFace: !this.state.showFace})
+
+  front = () => {
+    let pok = this.props.pokemon
     return (
       <Card>
-        <div>
+        <div onClick = {this.changeState}>
           <div className="image">
-            <img alt="oh no!" />
+            <img alt="oh no!" src = {pok.sprites.front}/>
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{pok.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {pok.stats.find(stat => stat.name === "hp").value}
             </span>
           </div>
         </div>
       </Card>
+    )
+  }
+
+  back = () => {
+    let pok = this.props.pokemon
+    return (
+      <Card>
+        <div onClick = {this.changeState}>
+          <div className="image">
+            <img alt="oh no!" src = {pok.sprites.back}/>
+          </div>
+          <div className="content">
+            <div className="header">{pok.name}</div>
+          </div>
+          <div className="extra content">
+            <span>
+              <i className="icon heartbeat red" />
+              {pok.stats.find(stat => stat.name === "hp").value}
+            </span>
+          </div>
+        </div>
+      </Card>
+    )
+  }
+
+  render() {
+    return (
+     this.state.showFace? this.front(): this.back()
     )
   }
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,57 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      filter: null,
+      pokemons: []
+    }
+  }
+
+  changeFilter = (event) => {
+    let filter = event.target.value
+    filter.length === 0 ? filter = null : filter
+    this.setState({filter})
+  }
+
+  componentDidMount(){
+    this.getPokemons()
+  }
+
+  getPokemons = () => {
+    fetch("http://localhost:3000/pokemon")
+      .then(resp => resp.json())
+      .then(pokemons => this.setState({pokemons}))
+  }
+
+  filterPokemons = () => {
+    return this.state.filter? this.state.pokemons.filter(p => p.name.includes(this.state.filter)) : this.state.pokemons 
+  }
+
+  addPokemon = (pokemon) =>{
+    let params = {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(pokemon)
+    }
+    fetch("http://localhost:3000/pokemon", params)
+      .then(resp => resp.json())
+      .then(json => this.setState({pokemons: [...this.state.pokemons, json]}))
+
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon = {this.addPokemon}/>
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search onChange={this.changeFilter} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons = {this.filterPokemons()} />
       </Container>
     )
   }
